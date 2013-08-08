ccecc3fe708389e435b9f67c15bf00f6039233e3
Thu Quach <thuquach90@gmail.com>
2013-08-08 12:08:12 -0700

    Playing with cloundmailin.

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,9 +2,12 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    subject = params[:headers][:Subject]
-    id      = subject.match(/\d+$/).to_s
-    reply   = params[:plain]
+    message = Mail.new(params[:message])
+    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
+    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
+
+    # Do some other stuff with the mail message
+
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
 end
