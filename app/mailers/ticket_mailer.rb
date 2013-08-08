a12cc912b1ec12f303995bf707b82dc37209a21d
Thu Quach <thuquach90@gmail.com>
2013-08-08 09:28:29 -0700

    Trying to fix things.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -4,7 +4,7 @@ class TicketMailer < ActionMailer::Base
   def new_ticket_notification(ticket)
     @ticket = ticket
 
-    headers["ticket_id"] = @ticket.id
-    mail to: ENV["HELPDESK_EMAIL"], subject: "New Ticket: #{@ticket.title} ID: #{@ticket.id}"
+    @url  = 'http://example.com/login'
+    mail(to: ENV['GMAIL_USERNAME'], subject: "New Ticket: #{@ticket.title} ID: #{@ticket.id}")
   end
 end
