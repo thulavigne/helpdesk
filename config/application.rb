7c2c6512b69400c694bf8c01a9308c4bee48f1cb
Thu Quach <thuquach90@gmail.com>
2013-08-08 09:07:31 -0700

    Changed application.rb in config.

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,5 @@ module Helpdesk
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
   end
 end
