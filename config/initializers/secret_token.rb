48f7477ea3972839e93e91957a6e27f9acfdb191
Thu Quach <thuquach90@gmail.com>
2013-08-07 19:33:39 -0700

    Generated Rails 3 App Helpdesk.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesk::Application.config.secret_token = 'f6ca2ea5b5fe955898a00e4273a416102ee9c881fbbdbbddd66eba4a8c4653f4587f9d8757dd0bd32e3292b93b9e30b4016d08b6d7949ee5ed8a8057b746aacd'
