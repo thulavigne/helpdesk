659248867a96c2efed9cca091e52d434c7c5f79a
Thu Quach <thuquach90@gmail.com>
2013-08-08 23:25:41 -0700

    Trying to make this work still.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,18 +14,7 @@ Helpdesk::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'helpdesk.com',
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -47,7 +36,3 @@ Helpdesk::Application.configure do
   config.assets.debug = true
 
 end
-
-# load dotenv
-require 'dotenv'
-Dotenv.load
