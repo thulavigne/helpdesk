ea157727ace2a1e1606faa9b7a63cefcd4162f44
Thu Quach <thuquach90@gmail.com>
2013-08-08 22:58:30 -0700

    Trying to get show view of tickets to work.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,86 +1,45 @@
 class TicketsController < ApplicationController
   before_filter :find_ticket, only: [:show, :edit, :update, :destroy]
 
-  # GET /tickets
-  # GET /tickets.json
   def index
     @tickets = Ticket.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @tickets }
-    end
   end
 
-  # GET /tickets/1
-  # GET /tickets/1.json
   def show
   end
 
-  # GET /tickets/new
-  # GET /tickets/new.json
   def new
     @ticket = Ticket.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @ticket }
-    end
   end
 
-  # GET /tickets/1/edit
   def edit
   end
 
-  # POST /tickets
-  # POST /tickets.json
   def create
     @ticket = Ticket.new(params[:ticket])
-
-    respond_to do |format|
-      if @ticket.save
-        # Tell the UserMailer to send a welcome Email after save
-        TicketMailer.new_ticket_notification(@ticket).deliver
-
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully created.' }
-        format.json { render json: @ticket, status: :created, location: @ticket }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
+    if @ticket.save
+      AdminTicket.new_ticket(@ticket).deliver
+      redirect_to @ticket, notice: 'Ticket was successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /tickets/1
-  # PUT /tickets/1.json
   def update
-    @ticket = Ticket.find(params[:id])
-
-    respond_to do |format|
-      if @ticket.update_attributes(params[:ticket])
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
+    if @ticket.update_attributes(params[:ticket])
+      redirect_to @ticket, notice: 'Ticket was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /tickets/1
-  # DELETE /tickets/1.json
   def destroy
     @ticket.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tickets_url }
-      format.json { head :no_content }
-    end
+    redirect_to tickets_url
   end
 
   private
   def find_ticket
     @ticket = Ticket.find(params[:id])
   end
-
 end
